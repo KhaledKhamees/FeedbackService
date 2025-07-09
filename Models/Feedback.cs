7f9aba4659b3ec516d9bd2c23a84312e8c573cc1
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2025-07-09 20:10:16 +0300

    Define application models

---
+++ Models/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeedbackService.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int FeedbackId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public Guid ItemId { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int Rating { get; set; }
+        [MaxLength(500, ErrorMessage = "Comment cannot exceed 500 characters")]
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Models/OutboxEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeedbackService.Models
+{
+    public class OutboxEvent
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string EventType { get; set; } = string.Empty;
+        [Required]
+        public string Payload { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool? Processed { get; set; }
+    }
+}
